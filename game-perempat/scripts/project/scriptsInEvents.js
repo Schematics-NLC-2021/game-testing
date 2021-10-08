6a1899ed8ad3d866231ae0e1c0c463041553c35b
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-10-08 21:53:19 +0700

    upd game-perempat: change login, add pop up, add save system

--- game-perempat/scripts/project/scriptsInEvents.js
+++ game-perempat/scripts/project/scriptsInEvents.js
@@ -3,20 +3,20 @@
 
 const scriptsInEvents = {
 
-		async Game_es_Event25_Act3(runtime, localVars)
+		async Game_es_Event26_Act3(runtime, localVars)
 		{
 			const soal = "contoh pertanyaan safkhdslk safsahlk saf";
 			
 			soalFrame.contentWindow.setSoal(soal);
 		},
 
-		async Game_es_Event28_Act1(runtime, localVars)
+		async Game_es_Event29_Act1(runtime, localVars)
 		{
 			console.log(localVars.jawaban);
 			console.log("jawaban diterima");
 		},
 
-		async Game_es_Event29_Act1(runtime, localVars)
+		async Game_es_Event30_Act1(runtime, localVars)
 		{
 			console.log(localVars.score)
 			console.log("score diterima")
