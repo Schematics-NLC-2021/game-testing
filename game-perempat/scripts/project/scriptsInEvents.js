7677af4cacd5565a384866d57f25abd1c9372002
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-10-15 23:40:06 +0700

    upd game-perempat: change pos game behaviour, add get score, disable log

--- game-perempat/scripts/project/scriptsInEvents.js
+++ game-perempat/scripts/project/scriptsInEvents.js
@@ -3,34 +3,12 @@
 
 const scriptsInEvents = {
 
-		async Game_es_Event31_Act1(runtime, localVars)
+		async Game_es_Event38_Act3(runtime, localVars)
 		{
 			sessionStorage.setItem("token", runtime.globalVars.token);
 			sessionStorage.setItem("curPos", runtime.globalVars.curPos);
 			sessionStorage.setItem("apiBase", runtime.globalVars.baseUrl);
 			sessionStorage.setItem("team_id", runtime.globalVars.id_team);
-		},
-
-		async Game_es_Event36_Act1(runtime, localVars)
-		{
-			console.log(localVars.soal_id1)
-			console.log(localVars.soal_id2)
-			console.log("id_soal diterima")
-		},
-
-		async Game_es_Event38_Act1(runtime, localVars)
-		{
-			console.log(localVars.score)
-			console.log("score soal diterima")
-			console.log(localVars.soal_id1)
-			console.log(localVars.soal_id2)
-			console.log("id_soal diterima")
-		},
-
-		async Game_es_Event41_Act1(runtime, localVars)
-		{
-			console.log(localVars.score)
-			console.log("score game diterima")
 		}
 
 };
